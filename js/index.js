58723d35de66beae113bf33673c2b6846d067e43
Sun Sreng <sun.sreng123@gmail.com>
2017-08-23 22:39:42 +0700

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+var myCat = {
+    "name": "A Black",
+    "Species": "Cat",
+    "FavFood": "Tuna"
+}
+console.log(myCat.FavFood);
+
+var myColor = ["Green", "White", "Blue"];
+console.log(myColor[2]);
+
+var thePets = [
+    {
+        "name": "A Black",
+        "Species": "Cat",
+        "FavFood": "Tuna"
+    },
+    {
+        "name": "A Second",
+        "Species": "Cat",
+        "FavFood": "Tuna"
+    },
+    {
+        "name": "A Third",
+        "Species": "Cat",
+        "FavFood": "Tuna"
+    }
+];
+console.log(thePets[1].name);
